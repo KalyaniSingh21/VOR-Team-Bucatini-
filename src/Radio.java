31711eddd31b516304c99a6d139a695bf0f16b7d
KalyaniSingh21 <kalyanisingh@mail.fresnostate.edu>
2014-11-08 19:42:56 -0800

    Major changes in getRadial function and Morse Code function. Changes made in Junit Code to comply with the changes done in Radio Class.

--- src/Radio.java
+++ src/Radio.java
@@ -1,66 +1,107 @@
-
 /**
- * Simulated Radio class.
+   Simulated Radio class.
 **/
 
-import java.util.Random;
+import java.io.*;
+import java.util.*;
 
 public class Radio 
 {
-	int seed;
-	int badCounter = 5;
-	int ID1 = seed + 767;
-	int ID2 = seed + 321;
+	//BufferedReader buffer;
+	//int seed;      This variable will be used in accord with future requirements
+	double frequency;    // The frequency to which the plane is set to in the beginning(VOR station)
+	//String id;           // The OBS desired radial returned by the VOR station
 	double startRad = 0;
-        
- // Radio constructor
-        
-	public Radio(int seed){
-		this.seed = seed;
-	}
- // Deciding the initial radial
-	public double getRadial(){
-		double radial;
-		Random dice = new Random();
-		radial = dice.nextDouble() * 3.45;
-		int tick = dice.nextInt(2);
-		if(tick > 0)
-                {
-			return startRad + radial;
-		}
-		return startRad + 360 - radial;
+	
+	// Radio constructor
+	public Radio(/*int seed */){
+	//	this.seed = seed;
 	}
 	
-	public int getID(){
-		Random rnd = new Random();
-		int a = rnd.nextInt(2);
-		if(a == 0){
-			return this.ID1;
-		}
-		return ID2;
+	// Deciding the initial radial
+    public int getRadial()
+    {
+    	int min = 0;
+    	int max = 359;
+    	
+    	int radial = min + (int)(Math.random() * (max - min + 1));   
+    	return radial;
+    }
+	
+	//Get Station ID
+        //@SuppressWarnings("empty-statement")
+       
+    @SuppressWarnings("empty-statement")
 
+    public String getMorseCode_sigQual(double frequency) throws IOException  // Initially getID
+    {    
+        try
+        {
+	   /* System.out.println("Enter the desired Frequency: ");
+            BufferedReader inp = new BufferedReader (new InputStreamReader(System.in));
+            frequency= Double.parseDouble(inp.readLine());
+          */
+	    double[] freq_arr = {115.4,113.5,114.8,113.9,116.1,117.7,
+                                        115.1,112.3,113.3,116.9,112.1};
+            String[] MorseCode = {"SOK","LIH","HNL","CKH","MKK","LNY",
+                                         "OGG","UPP","MUE","ITO","KOA"};
+                       
+            for(int i=0;i<=11;i++)
+            {
+                if(frequency==freq_arr[i])
+                   return (MorseCode[i]);
+                
+            }
+        
+        
+        }
+    
+        catch(NumberFormatException e)
+        {
+            System.out.println("Number Format Exception");
+        
+        }
+        catch(Exception e)
+        {
+            System.out.println("Exception");
         }
         
-// Checking the quality of the signal
         
-     	public String getSigQual(){
-		if(badCounter <= 0){
-			return "";
-		}
-		if(this.badCounter < 5){
-			this.badCounter--;
-			//System.out.println("bad counter = " + this.badCounter);
-			return "bad";
-		}
+        //return MorseCode[0];
+            return null;
+   
+    }
+ 
+    //private String[] ret;
+	
+	//Checking the quality of the signal
+    public String getQual()
+    {
+    	Random rnd = new Random();
+	int n = rnd.nextInt(10);
+		    	
+    	if(n==1 || n == 6 || n == 8)
+        {
+            System.out.println("Number = " + n);
+	    return "bad";
+	}
 		
-		Random rn4 = new Random();
-		int a = rn4.nextInt(3);
-		if(a == 0){
-			this.badCounter--;
-			//System.out.println("bad counter = " + this.badCounter);
-			return "bad";
-		}
-		return "";
-	}   
-        
+	else
+        {
+            System.out.println("Number = " + n);
+	    return "good";
+	} 
+    }
+    
+    
+    /*
+    public static void main(String[] args) throws Exception
+    {
+    	Radio r = new Radio();
+    	System.out.print(r.getMorseCode_sigQual()+","+
+    	r.getQual()+","+
+    	r.getRadial());
+    } */
 }
+
+
---
+++ test/RadioTest.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Kalyani
+ */
+public class RadioTest {
+    
+    public RadioTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getRadial method, of class Radio.
+     */
+    @Test
+    public void testGetRadial() {
+        System.out.println("getRadial");
+        Radio instance = new Radio();
+       // double expResult = 0.0;
+        double result = instance.getRadial();
+        
+        if((result>=0) && (result <=360))
+           System.out.print("Test Passed");  
+        else
+            System.out.print("Test Failed");
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+    
+    
+    
+    /**
+     * Test of getMorseCode_sigQual method, of class Radio.
+     * @param frequency
+     * @throws java.lang.Exception
+     */
+    @Test
+
+    public void testGetMorseCode_sigQual(double frequency) throws IOException  
+    {
+        System.out.println("getMorseCode_sigQual");
+        Radio instance = new Radio();
+        String expResult = "";
+        double[] freq_arr = {115.4,113.5,114.8,113.9,116.1,117.7,
+                                        115.1,112.3,113.3,116.9,112.1};
+            String[] MorseCode = {"SOK","LIH","HNL","CKH","MKK","LNY",
+                                         "OGG","UPP","MUE","ITO","KOA"};
+                       
+            for(int i=0;i<=11;i++)
+            {
+                if(frequency==freq_arr[i])
+                   expResult = (MorseCode[i]);
+                
+            }
+        
+        String result = instance.getMorseCode_sigQual(frequency);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+    
+ 
+    
+    }
+    
+
