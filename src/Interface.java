f7884a800d0cae517945319f2e73a569e0b69c32
pritamdhanoa <pritamdhanoa@mail.fresnostate.edu>
2014-12-05 00:22:09 -0800

    Added some more elements to Interface

--- src/Interface.java
+++ src/Interface.java
@@ -16,22 +16,51 @@ public class Interface extends JFrame implements ActionListener
         
         JPanel panel = new JPanel();
 	
-	JButton start = new JButton("Start Simulation");
-	JButton quit = new JButton("Quit Simulation");
+        
+        JLabel lobs = new JLabel("OBS");
+        JTextField tobs = new JTextField();
+        
+        JLabel lradial = new JLabel("Radial");
+        JTextField tradial = new JTextField();
+        
+        JLabel ltofrom = new JLabel("To/From");
+        JTextField ttofrom = new JTextField();
+        
+        JLabel lbad = new JLabel("Good/Bad");
+        JTextField tbad = new JTextField();
+                
+	JButton start = new JButton("Start");
+	JButton quit = new JButton("Quit");
 	JButton left = new JButton("Left");
 	JButton right = new JButton("Right");
-		
+        
 	public Interface() 
 	{  
 		super("VOR"); 
 		
 		cnt.setLayout(null);
                 cnt.setBackground(white);
-		setSize(500,500);  
+		setSize(500,500);
+                setVisible(true);  
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 
-                cnt.add(panel,BorderLayout.PAGE_END);
+                panel.setLayout(null);
+		panel.setBackground(white);
+		panel.setEnabled(true);
+		panel.setVisible(true);
+                        
+                cnt.add(panel);
+                panel.setBounds(0,300,500,200);
 		
+                panel.add(lobs);
+		panel.add(tobs);
+                panel.add(lradial);
+		panel.add(tradial);
+		panel.add(ltofrom);
+		panel.add(ttofrom);
+		panel.add(lbad);
+		panel.add(tbad);
+                
                 start.addActionListener(this);
 		panel.add(start);
 		quit.addActionListener(this);
@@ -40,6 +69,19 @@ public class Interface extends JFrame implements ActionListener
 		panel.add(left);
 		right.addActionListener(this);
 		panel.add(right);
+                
+                lobs.setBounds(50,50,75,25);
+		tobs.setBounds(150,50,75,25);
+		lradial.setBounds(250,50,75,25);
+		tradial.setBounds(350,50,75,25);
+		ltofrom.setBounds(50,80,75,25);
+		ttofrom.setBounds(150,80,75,25);
+		lbad.setBounds(250,80,75,25);
+		tbad.setBounds(350,80,75,25);
+		left.setBounds(50,110,75,25);
+		right.setBounds(150,110,75,25);
+		start.setBounds(250,110,75,25);
+		quit.setBounds(350,110,75,25);
     
 	}
         
