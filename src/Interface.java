7d28d639bcf50dd9186d10c4e2bfff0a5035a739
pritamdhanoa <pritamdhanoa@mail.fresnostate.edu>
2014-12-05 16:00:19 -0800

    Fixed the bad signal and to/from functions

--- src/Interface.java
+++ src/Interface.java
@@ -12,11 +12,21 @@ public class Interface extends JFrame implements ActionListener
 {  	
 	Container cnt= getContentPane();
 
+        Radio radio = new Radio(0);
+                
+        VOR r = new VOR(radio.getRadial());
+            
+        
 	Color white = new Color(255,255,255);
         
         JPanel panel = new JPanel();
 	
+        JLabel lfreq = new JLabel("Frequency");
+        JTextField tfreq = new JTextField();
         
+        JLabel lid = new JLabel("ID");
+        JTextField tid = new JTextField();
+                
         JLabel lobs = new JLabel("OBS");
         JTextField tobs = new JTextField();
         
@@ -30,15 +40,15 @@ public class Interface extends JFrame implements ActionListener
         JTextField tbad = new JTextField();
                 
 	JButton start = new JButton("Start");
-	JButton quit = new JButton("Quit");
+	JButton stop = new JButton("Stop");
 	JButton left = new JButton("Left");
 	JButton right = new JButton("Right");
         
 	public Interface() 
-	{  
-		super("VOR"); 
+	{
+                super("VOR"); 
 		
-		cnt.setLayout(null);
+       		cnt.setLayout(null);
                 cnt.setBackground(white);
 		setSize(500,500);
                 setVisible(true);  
@@ -52,6 +62,10 @@ public class Interface extends JFrame implements ActionListener
                 cnt.add(panel);
                 panel.setBounds(0,300,500,200);
 		
+                panel.add(lfreq);
+		panel.add(tfreq);
+                panel.add(lid);
+		panel.add(tid);
                 panel.add(lobs);
 		panel.add(tobs);
                 panel.add(lradial);
@@ -63,13 +77,17 @@ public class Interface extends JFrame implements ActionListener
                 
                 start.addActionListener(this);
 		panel.add(start);
-		quit.addActionListener(this);
-		panel.add(quit);
+		stop.addActionListener(this);
+		panel.add(stop);
 		left.addActionListener(this);
 		panel.add(left);
 		right.addActionListener(this);
 		panel.add(right);
                 
+                lfreq.setBounds(50,20,75,25);
+		tfreq.setBounds(150,20,75,25);
+		lid.setBounds(250,20,75,25);
+		tid.setBounds(350,20,75,25);
                 lobs.setBounds(50,50,75,25);
 		tobs.setBounds(150,50,75,25);
 		lradial.setBounds(250,50,75,25);
@@ -81,7 +99,7 @@ public class Interface extends JFrame implements ActionListener
 		left.setBounds(50,110,75,25);
 		right.setBounds(150,110,75,25);
 		start.setBounds(250,110,75,25);
-		quit.setBounds(350,110,75,25);
+		stop.setBounds(350,110,75,25);
     
 	}
         
@@ -89,44 +107,78 @@ public class Interface extends JFrame implements ActionListener
         {
 	        if(ae.getSource() == left)
                 {
+                    left();
 		}
 		
                 if(ae.getSource() == right)
                 {
+                    right();
 		}
 		
-                if(ae.getSource() == quit)
+                if(ae.getSource() == stop)
                 {
+                    clear();
 		}
 		
                 if(ae.getSource() == start)
                 {
-                    update();
+                    initial();
 		}
 	}
         
-        public void update()
-        {
-                VOR r = new VOR();
-            
+        public void initial()
+        {   
+                radio.setFrequency(Double.parseDouble(tobs.getText()));
                 r.setOBS(Integer.parseInt(tobs.getText()));
             
+                update();
+        }
+        
+        public void left()
+        {
+                r.setOBS(Integer.parseInt(tobs.getText())-1);
+ 
+                update();
+        }
+        
+        public void right()
+        {           
+                r.setOBS(Integer.parseInt(tobs.getText())+1);
+ 
+                update();
+        }
+        
+        public void clear()
+        {
+                tfreq.setText("");
+                tid.setText("");
+                tobs.setText("");
+                tradial.setText("");
+                ttofrom.setText("");
+                tbad.setText("");
+        }
+        
+        public void update()
+        {
+                tid.setText(radio.getMorseCode());
+                tobs.setText(Integer.toString(r.getOBS()));
                 tradial.setText(Integer.toString(r.intercepted));
                 ttofrom.setText(r.isGoingTo());
-                tbad.setText(r.isSignalGood());
+                tbad.setText(r.isSignalGood(radio.badSigQual()));
 
                 System.out.println(r.intercepted + ", "
                 + r.getCDI() + ", "
                 + r.isGoingTo() + ", "
-                + r.isSignalGood());
-        
+                + r.isSignalGood(radio.badSigQual()));
+            
         }
-		
+	
 
 	public static void main(String [] args)  
 	{
 		Interface inter = new Interface();    	
     
+                
 	}
 
 }
